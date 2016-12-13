4dc524d811db1eaca243daa79fe295c6846f9517
nsmith15PCC <nsmith15@go.pasadena.edu>
2016-12-12 16:13:48 -0800

    Updated CSVDemo

---
+++ CSVParser/CSVDemo.java
@@ -0,0 +1,28 @@
+package CSVParser;
+
+import java.util.List;
+import java.io.IOException;
+
+class CSVDemo {
+    private static final String filename = "airports.dat";
+
+    public static void main(String[] args) throws IOException {
+        CSVParser par;
+        try {
+            par = new CSVParser(filename);
+        }
+        catch(IOException e) {
+            System.out.println("Error opening file.");
+            throw(e);
+        }
+        for(int i = 0; i < 10; ++i) {
+            List<String> l;
+            l = par.getNext();
+            for (String s : l) {
+                System.out.print(s + "&&&");
+            }
+            System.out.println();
+        }
+    }
+
+}
